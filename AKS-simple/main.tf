4f159ca0750c3ea54e59c6b4a8f58a398f1bf68e
lkolo-prez <85799218+lkolo-prez@users.noreply.github.com>
2023-06-16 17:44:06 +0200

    Create main.tf

---
+++ AKS-simple/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.46"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name       = var.node_pool_name
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
